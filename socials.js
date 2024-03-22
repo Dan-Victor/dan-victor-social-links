984297c0bce698a601fb35fda7caeb23852cc77b
Dan-Victor <141946753+Dan-Victor@users.noreply.github.com>
2024-03-22 21:12:47 +0100

    Add files via upload

---
+++ socials.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function(){
+    //for  all links
+let socialLinks = document.querySelectorAll(".social-link");
+socialLinks.forEach((element)=>{
+element.addEventListener('mouseover', ()=>{
+    element.style.color = "#000"
+element.style.backgroundColor = "hsl(75, 94%, 57%)"
+})
+
+element.addEventListener('mouseout', ()=>{
+    element.style.removeProperty('color')
+    element.style.removeProperty('background-color')
+    })
+})
+ //for individual links
+ let gitHub = socialLinks[0];
+    let linkedIn = socialLinks[1];
+    let Instagram = socialLinks[2];
+    let Twitter = socialLinks[3];
+    let snapChat = socialLinks[4];
+    //for gitHub
+ gitHub.addEventListener('click' , ()=>{
+    window.open('https://www.github.com/dan-victor',"_blank")
+ })
+ //for linkedIn
+ linkedIn.addEventListener('click' , ()=>{
+    window.open('https://www.linkedin.com/in/dan-victor-adesola-717bb5299',"_blank")
+ })
+ //for Instagram
+ Instagram.addEventListener('click' , ()=>{
+    window.open('https://www.instagram.com/danvictor_ades?igsh=MXVwdGkyMXl1YmlybQ==',"_blank")
+ })
+ //for Twitter
+ Twitter.addEventListener('click' , ()=>{
+    window.open('https://x.com/Dn_vico?t=2ktAA8Bb6qyTeslZGH_XSG&S=09',"_blank")
+ })
+ //for snapChat
+ snapChat.addEventListener('click' , ()=>{
+    window.open('https://www.github.com/dan-victor',"_blank")
+ })
+})
